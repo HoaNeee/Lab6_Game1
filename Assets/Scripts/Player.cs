3497617f8df6105fbb3247596d66f56b94498627
DELL <nhhoa03@gmail.com>
2024-01-26 09:39:56 +0700

    daycode

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject panelEndGame;
+    public GameObject panelWinGame;
+
+    Rigidbody2D rigidbody2D;
+
+    public int cherry = 0;
+    public TextMeshProUGUI cherryText;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    public void Jump ()
+    {
+        Debug.Log("Vao ham Jump");
+        rigidbody2D.AddForce(Vector2.up * 6, ForceMode2D.Impulse);
+
+        
+    }
+
+    public void RestartGame ()
+    {
+        panelEndGame.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void WinGame()
+    {
+        panelWinGame.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "obstacles")
+        {
+            panelEndGame.SetActive(true);
+            Time.timeScale = 0;
+        }
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("item"))
+        {
+            cherry++;
+            cherryText.SetText(cherry.ToString());
+            Destroy(collision.gameObject);
+
+            if(cherry >= 6)
+            {
+                panelWinGame.SetActive(true);
+                Time.timeScale = 0;
+            }
+        }
+    }
+    public void MoveLeft()
+    {
+        
+        rigidbody2D.AddForce(Vector2.left* 5, ForceMode2D.Impulse);
+    }
+
+    public void MoveRight()
+    {
+        rigidbody2D.AddForce(Vector2.right * 5, ForceMode2D.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
